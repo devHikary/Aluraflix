5590ce48941f1036f5e69890ccc2835facc2423a
devHikary <wrkkarina@gmail.com>
2021-09-08 21:05:28 -0300

    Configuração do H2 para testes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO VIDEO(titulo, descricao, url) VALUES('Video 1', 'descricao do video 1', 'url.do.video.1');
+INSERT INTO VIDEO(titulo, descricao, url) VALUES('Video 2', 'descricao do video 2', 'url.do.video.2');
+INSERT INTO VIDEO(titulo, descricao, url) VALUES('Video 3', 'descricao do video 3', 'url.do.video.3');
+INSERT INTO VIDEO(titulo, descricao, url) VALUES('Video 4', 'descricao do video 4', 'url.do.video.4');
